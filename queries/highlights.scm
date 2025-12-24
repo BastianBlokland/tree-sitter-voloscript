7485d2db009f46d8cf9870ebaf3ca715425b78d1
Bastian Blokland <mail@bastian.tech>
2025-12-24 19:11:08 +0200

    Add a highlights query

---
+++ queries/highlights.scm
@@ -0,0 +1,34 @@
+
+(expression_if)       @keyword.control
+(expression_if
+  "else" @keyword.control
+)
+
+(expression_while)    @keyword.control
+(expression_for)      @keyword.control
+(expression_return)   @keyword.control
+(expression_continue) @keyword.control
+(expression_break)    @keyword.control
+(expression_var_declare) @keyword.declaration
+
+(constant)            @constant.builtin
+(number)              @number
+(string)              @string
+
+(identifier)          @variable
+(key)                 @variable.builtin
+
+(expression_call
+  function: (identifier) @function.call
+)
+
+(expression_modify)   @operator.assignment
+(expression_binary)   @operator.arithmetic
+(expression_unary)    @operator.arithmetic
+(expression_select)   @operator.ternary
+
+(block_explicit)      @punctuation.bracket
+(expression_paren)    @punctuation.bracket
+
+(comment)   @comment
+(separator) @punctuation.separator
