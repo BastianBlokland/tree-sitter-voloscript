2811ba0095e2044cae4db48d140923275f60c0a7
Bastian Blokland <mail@bastian.tech>
2025-12-27 15:13:30 +0200

    Add nix build definition

--- flake.nix
+++ flake.nix
@@ -5,12 +5,21 @@
     nixpkgs.url = "nixpkgs/nixos-25.11";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
   in
   {
+    packages.${system}.tree-sitter-voloscript =
+      pkgs.tree-sitter.buildGrammar {
+        language = "voloscript";
+        version = "0.1.0";
+        src = self;
+      };
+
+    defaultPackage.${system} = self.packages.${system}.tree-sitter-voloscript;
+
     devShells.${system}.default = pkgs.mkShell {
       packages = [
         pkgs.gcc
